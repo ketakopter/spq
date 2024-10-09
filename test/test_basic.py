75d26ab090ec148a6ce9d5a7ef60ba6701f43adc
ketakopter <nomail@nourl.com>
2024-10-09 22:53:14 +0200

    Extended testing of building blocks

--- test/test_basic.py
+++ test/test_basic.py
@@ -1,11 +1,17 @@
 import unittest
+from pathlib import Path
 
 import numpy as np
 
 from spq.gr import Gr
-from spq.spq import createPq
+from spq.spq import createPq, createPqFromDict, createPqsFromJsonFile
+from spq.spq import graphOfLinearScaling
 
-class TestGraph(unittest.TestCase):
+curr_dir = Path(__file__).parent
+res_dir = curr_dir.joinpath('resources')
+test_file = res_dir.joinpath('units.json')
+
+class TestSpqCreation(unittest.TestCase):
 
     def test_graph(self):
 
@@ -36,3 +42,57 @@ class TestGraph(unittest.TestCase):
         self.assertAlmostEqual(Dist.fromft(3.53).km, 0.001075944)
         self.assertAlmostEqual(Dist.fromexpm(3).m, np.log(3))
         self.assertAlmostEqual(Dist(2).expm, np.exp(2))
+
+    def test_dict(self):
+
+        dct = {'name': 'Dist',
+               'description': 'distance',
+               'main_unit': 'm',
+               'conversions': [
+                   {'from': 'm', 'to': 'ft', 'factor': 3.28},
+                   {'from': 'km', 'to': 'm', 'factor': 1000.0},
+                   {'from': 'sm', 'to': 'm', 'factor': 1.0, 'origin': 500.0}
+               ]
+               }
+
+        Dist = createPqFromDict(dct)
+
+        self.assertAlmostEqual(Dist.fromft(3.28), 1)
+        self.assertAlmostEqual(Dist.fromft(3.28).km, 1e-3)
+        self.assertAlmostEqual(Dist.fromsm(0), 500.0)
+        self.assertAlmostEqual(Dist(0).sm, -500.0)
+        self.assertAlmostEqual(Dist.fromkm(1).sm, 500.0)
+
+    def test_json(self):
+
+        pqs = createPqsFromJsonFile(test_file)
+
+        self.assertEqual(sorted([q._name for q in pqs]), ['Dist', 'Vol'])
+
+        Dist, Vol = pqs
+
+        self.assertEqual(sorted(Dist._units), ['digitus', 'palmus', 'passus', 'pes', 'stadium'])
+        self.assertEqual(sorted(Vol._units), ['amphora', 'congius', 'quartarius'])
+
+        a = Dist.frompes(1)
+
+        self.assertAlmostEqual(a.digitus, 16.0)
+        self.assertAlmostEqual(a.palmus, 4.0)
+        self.assertAlmostEqual(a.pes, 1.0)
+        self.assertAlmostEqual(a.passus, 0.2)
+        self.assertAlmostEqual(Dist.frompassus(1).digitus, 80.0)
+
+        b = Vol.fromamphora(10)
+
+        self.assertAlmostEqual(b.congius, 80.0)
+        self.assertAlmostEqual(b.quartarius, 1920.0)
+
+class TestLinearGraph(unittest.TestCase):
+
+    def test_init(self):
+
+        factors = [('km', 'm', 1.e3), ('m', 'ft', 3.28)]
+
+        dist_graph = graphOfLinearScaling(factors)
+
+        self.assertEqual(dist_graph.findPath("km", "ft"), ['km', 'm', 'ft'])
