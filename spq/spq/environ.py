e2b68e08e33fe4ee4b3d0f214e9c91089fe8d0c1
ketakopter <nomail@nourl.com>
2024-10-14 23:44:28 +0200

    More helpful exception message

--- spq/spq/environ.py
+++ spq/spq/environ.py
@@ -4,7 +4,9 @@ import os
 from spq.base.pq import createPqsFromJsonFile
 
 # Load magnitudes from external Json.
-pqFile = os.environ['SPQFILE']
+pqFile = os.environ.get('SPQFILE', None)
+if not pqFile:
+    raise KeyError("SPQFILE environment variable not set")
 pqObjs = createPqsFromJsonFile(pqFile)
 
 # Add the symbols to the package.
